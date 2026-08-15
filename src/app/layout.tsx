c3487db2d3961f2185f43ca1f4af2cb128312559
Noctyris <noctyris@proton.me>
2026-08-15 08:46:50 +0200

    Create header and main parts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,41 +5,49 @@ import { auth } from "@root/auth"
 import "./globals.css";
 
 const syncopate = Syncopate({
-  weight: ['400', '700'],
-  subsets: ['latin'],
-  variable: '--font-syncopate',
+    weight: ['400', '700'],
+    subsets: ['latin'],
+    variable: '--font-syncopate',
 });
 
 const sora = Sora({
-  subsets: ['latin'],
-  variable: '--font-sora',
+    subsets: ['latin'],
+    variable: '--font-sora',
 });
 
 const geist = Geist({
-  subsets: ['latin'],
-  variable: '--font-geist',
+    subsets: ['latin'],
+    variable: '--font-geist',
 });
 
 export const metadata: Metadata = {
-  title: "Circumpolar",
-  description: "",
+    title: "Circumpolar",
+    description: "",
 };
 
 export default async function RootLayout({
-  children, modal
+    children, modal
 }: Readonly<{
-  children: React.ReactNode;
-  modal: React.ReactNode;
+    children: React.ReactNode;
+    modal: React.ReactNode;
 }>) {
-  const session = await auth();
+    const session = await auth();
 
-  return (
-    <html lang="fr" className={`${syncopate.variable} ${sora.variable} ${geist.variable} dark`}>
-      <body
-        className="font-geist antialiased text-white"
-      >
-        <SessionProvider session={session}>{children}{modal}<div id="modal-root" /></SessionProvider>
-      </body>
-    </html>
-  );
+    return (
+        <html lang="fr" className={`${syncopate.variable} ${sora.variable} ${geist.variable} dark`}>
+            <body
+                className="font-geist antialiased text-white"
+            >
+                <SessionProvider session={session}>{modal}<div id="modal-root" />
+                    <header className="max-w-7xl mx-auto p-4 pb-0">
+                        <h1 className="font-title text-3xl md:text-5xl uppercase tracking-[0.2em] text-transparent bg-clip-text bg-gradient-to-b from-white to-white/40 m-4">Circumpolar</h1>
+
+                    </header>
+                    <main className="max-w-7xl mx-auto p-4">
+                        {children}
+                    </main>
+                </SessionProvider>
+            </body>
+        </html>
+    );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,10 +7,9 @@ export default async function Home() {
   const raw_images = await fetchImages();
 
   return (
-    <main className="max-w-7xl mx-auto p-4">
-      <h1 className="font-title text-3xl md:text-5xl uppercase tracking-[0.2em] text-transparent bg-clip-text bg-gradient-to-b from-white to-white/40 m-4">Circumpolar</h1>
+    <>
       <Grid images={raw_images} />
-    </main>
+    </>
   );
 }
 
