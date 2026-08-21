43dc07042b26aee66305be13162e1c96ca3caf26
Noctyris <noctyris@proton.me>
2026-08-21 23:23:19 +0200

    Update login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -15,20 +15,22 @@ export default function LoginPage() {
     // p-px bg-gradient-to-b from-gray-200 to-transparent
 
     return (
-        <div className="flex items-center justify-center rounded-3xl p-px bg-gradient-to-b from-gray-200 to-transparent">
-            <div className="">
-                <form action={formAction}>
+        <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+            <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+                <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-white">Connexion nécessaire</h2>
+            </div>
+            <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                <form action={formAction} className="space-y-6">
                     <div>
-                        <h1>Please log in to continue</h1>
-                    </div>
-                    <div>
-                        <div>
-                            <label htmlFor="password">Password</label>
-                            <input type="password" name="password" placeholder="password" required minLength={6} />
+                        <label htmlFor="password" className="block text-sm/6 font-medium text-gray-100">Mot de passe</label>
+                        <div className="mt-2">
+                            <input type="password" name="password" required minLength={6} className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-white/50 sm:text-sm/6" />
                         </div>
                     </div>
                     <input type="hidden" name="redirectTo" value={callbackUrl} />
-                    <button aria-disabled={isPending}>Connexion</button>
+                    <div>
+                        <button type="submit" aria-disabled={isPending} className="flex w-full justify-center rounded-md bg-white/50 px-3 py-1.5 text-sm/6 font-semibold text-white hover:bg-white/70 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white/70">Connexion</button>
+                    </div>
                     <div aria-live="polite" aria-atomic="true">
                         {errorMessage && (
                             <p className="text-red-500">{errorMessage}</p>
