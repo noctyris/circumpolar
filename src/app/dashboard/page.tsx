2dd586b345917ea10b9d8f441cd33832160d444b
Noctyris <noctyris@proton.me>
2026-08-21 23:38:31 +0200

    Add button to dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,11 @@
+import Link from "next/link"
+
 export default function DashboardPage() {
     return (
-        <span>Hi</span>
+        <>
+            <Link className="fixed flex justify-center items-center text-4xl bottom-6 right-6 w-12 rounded-full h-12 duration-300 bg-white/50 hover:bg-white/70 hover:text-6xl" href="dashboard/new">+</Link>
+            {/*  TODO: NEW SECTION INCOMING  */}
+            <code className="text-white/60 italic tracking-wider">Nothing here yet... except this button</code>
+        </>
     )
 }
