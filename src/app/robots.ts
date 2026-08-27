d899ebd16513c54b8bb46dd2849ecf4d90560409
Noctyris <126427514+noctyris@users.noreply.github.com>
2026-08-27 19:05:36 +0200

    Update robots.ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -5,7 +5,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: '*',
       allow: '/',
-      disallow: '/dashboard/',
+      disallow: ['/dashboard/', '/api/', '/login'],
     },
     sitemap: 'https://circumpolar.vercel.app/sitemap.xml',
   }
