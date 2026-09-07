89f45da5b90ccd2cc24dd1f29c3e7520e35505ce
Noctyris <126427514+noctyris@users.noreply.github.com>
2026-09-07 16:06:27 +0200

    Move button to avoid covering footer

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,9 +3,17 @@ import Link from "next/link"
 export default function DashboardPage() {
     return (
         <>
-            <Link className="fixed flex justify-center items-center text-4xl bottom-6 right-6 w-12 rounded-full h-12 duration-300 bg-white/50 z-99 hover:bg-white/70 hover:text-6xl" href="dashboard/new">+</Link>
             {/*  TODO: NEW SECTION INCOMING  */}
-            <code className="text-white/60 italic tracking-wider">Nothing here yet... except this button</code>
+            <code className="text-white/60 italic h-500 block tracking-wider">Nothing here yet... except this button</code>
+
+            <div className="sticky bottom-6 self-end pointer-events-none z-30 mt-auto">
+                <Link
+                    href="dashboard/new"
+                    className="pointer-events-auto flex justify-center items-center text-4xl w-12 h-12 rounded-full duration-300 bg-white/50 hover:bg-white/70 hover:text-6xl shadow-lg -mr-7"
+                >
+                    +
+                </Link>
+            </div>
         </>
     )
 }
